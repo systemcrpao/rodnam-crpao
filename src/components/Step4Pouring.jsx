0b5ddd85930a9834ff7673965990bb6ee4e87785
Kng-T <traipop.py@gmail.com>
2026-04-09 13:20:15 +0700

    update pouringGif pc

--- src/components/Step4Pouring.jsx
+++ src/components/Step4Pouring.jsx
@@ -10,7 +10,7 @@ export default function Step4Pouring() {
         <img
           src={pouringGif}
           alt="กำลังรดน้ำดำหัว"
-          className="w-96 sm:w-96 object-contain drop-shadow-2xl"
+          className="w-96 sm:w-full object-contain drop-shadow-2xl"
         />
       </div>
     </div>
